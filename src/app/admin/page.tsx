204476cc99fdfcfea3adf988ac82d9d0e170b936
Riyanshu Baskey <rbaskeyofficial@gmail.com>
2025-08-22 22:57:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@
 
 import * as React from 'react';
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
