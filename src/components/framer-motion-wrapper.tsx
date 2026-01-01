7a33338aaa93a73b7106ba881fe13034086ee41f
Riyanshu Baskey <rbaskeyofficial@gmail.com>
2026-01-01 22:10:35 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/framer-motion-wrapper.tsx
+++ src/components/framer-motion-wrapper.tsx
@@ -1,16 +1,21 @@
-
 'use client';
 
 import { motion, AnimatePresence } from 'framer-motion';
 import { useThemeManager } from '@/context/theme-provider';
+import { useState, useEffect } from 'react';
 
 export const FramerMotionWrapper = ({ children }: { children: React.ReactNode }) => {
   const { accentColor } = useThemeManager();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        key={accentColor.name}
+        key={isMounted ? accentColor.name : 'initial'}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
